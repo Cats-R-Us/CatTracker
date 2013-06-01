5426d3dd5c2c271568128c808c109890c11d88cb
Jayml <jayml.mistry@gmail.com>
2013-06-01 12:57:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatTracker::Application.config.secret_token = '216b181dfaca13f38e8cfe1ec73286247498802d1563e29657833374cb8293484b156f566459e6ef0de065d4438405a4efeea28e1ccdaa2c155d1f65a748f2a7'
